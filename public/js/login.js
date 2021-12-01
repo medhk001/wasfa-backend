ad8961cc36440233c2bf22efca2ace2d87c3dfd9
medhk001 <m.hakkou18@gmail.com>
2021-12-01 19:40:35 +0100

    comit1

---
+++ public/js/login.js
@@ -0,0 +1,42 @@
+console.clear();
+
+const loginBtn = document.getElementById('login');
+const signupBtn = document.getElementById('signup');
+const sign_up = document.getElementById('sign-up');
+const sign_in = document.getElementById('sign-in');
+
+loginBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode.parentNode;
+	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			signupBtn.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
+
+signupBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode;
+	Array.from(e.target.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			loginBtn.parentNode.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
+
+
+sign_up.addEventListener('click',  function() {
+	
+
+});
+
+
+sign_in.addEventListener('click',  function() {
+	
+
+});
